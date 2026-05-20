52789165853ae7e1a2c9c481fa0216d1623dcb19
PureLeaf-Tea <1014635782@QQ.COM>
2026-05-20 21:09:31 +0800

    Auto update: 2026/05/20 周三 21:09:31.11

--- deploy.bat
+++ deploy.bat
@@ -3,6 +3,6 @@ cd /d C:\Users\Administrator\Desktop\tea-website
 git remote set-url origin https://PureLeaf-Tea:ghp_UwfMzFBkPtDSgOd5kbCAH4g8EYHADC2h1DsJ@github.com/PureLeaf-Tea/tea-website.git
 git add .
 git commit -m "Auto update: %date% %time%"
-git push origin main 2>nul || git push origin master
+git push origin main
 echo Push finished! Cloudflare deploy in 1min
 pause
